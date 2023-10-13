ca38edac39ff5246c42af8c92dc09819878628f8
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2023-10-13 13:58:45 +0300

    Add project files.

---
+++ WPFHomework1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFHomework1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeButtonColor(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            int r = new Random().Next(0,256);
+            int g = new Random().Next(0,256);
+            int b = new Random().Next(0,256);
+
+            btn.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+            MessageBox.Show($"This button name: {btn.Name}\nButton text: {btn.Content}\nButton Background {btn.Background.ToString()}" +
+                $"\nButton foreground: {btn.Foreground}\n Button fontsize: {btn.FontSize}");
+
+        }
+
+        private void deleteButton(object sender, MouseButtonEventArgs e)
+        {
+            Button button = sender as Button;
+            this.Title = button.Content.ToString();
+            Grid grid = (Grid)button.Parent;
+           
+            grid.Children.Remove(button);
+        }
+    }
+}
